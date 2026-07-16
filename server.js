46ccd534a4389e8d81f934f7f73b7371f5bc1730
Mugenzijmv <mdesignpro10@gmail.com>
2026-07-17 00:22:11 +0200

    UI Changes

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
+const dns = require('node:dns');
+dns.setServers(['8.8.8.8', '1.1.1.1']);
+
+
 require("dotenv").config();
 
 const app = require("./app");
-const serverless = require("serverless-http");
 
-module.exports = serverless(app);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
+
+
+// require("dotenv").config();
+
+// const app = require("./app");
+// const serverless = require("serverless-http");
+
+// module.exports = serverless(app);
